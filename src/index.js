169f734c4cb171d0ff481af6b72a1899e15d980f
kkt9102 <kkt9102@gmail.com>
2022-10-31 23:19:52 +0900

    라우터 수정

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./resources/css/common.css";
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
